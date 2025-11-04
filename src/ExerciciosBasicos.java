434df12b478b5a0725a43a033d4978cbf5596186
Elieu <0001155168@senaimgaluno.com.br>
2025-11-04 09:36:31 -0300

    alteracao no bem vindo

--- src/ExerciciosBasicos.java
+++ src/ExerciciosBasicos.java
@@ -5,7 +5,7 @@ public class ExerciciosBasicos {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Hello, World!");
+        System.out.println("Hello elieu!");
 
     }
 

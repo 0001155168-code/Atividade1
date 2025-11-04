c7c7cebb42035fadac13b2d74c1ca78d152eebb8
Elieu <0001155168@senaimgaluno.com.br>
2025-11-04 09:31:39 -0300

    primeiro commit

---
+++ src/Calculadora.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Calculadora {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.nextFloat();
+    }
+}
